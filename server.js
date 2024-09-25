b82f5bb6e442af1d7e6d08e8a995c201b672c36b
Mohammed Afzal <78775949+Afzal-spec@users.noreply.github.com>
2024-09-25 15:07:11 +0530

    fixing bugs

--- server.js
+++ server.js
@@ -3,37 +3,48 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from 'morgan';
 import connectDB from "./config/db.js";
-import authRoutes from './routes/authRoute.js'
-import categoryRoutes from './routes/categoryRoutes.js'
-import productRoutes from './routes/productRoutes.js'
-import cors from 'cors'
-//configure env
+import authRoutes from './routes/authRoute.js';
+import categoryRoutes from './routes/categoryRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import cors from 'cors';
+import path from 'path'; // Import path for serving static files
+
+// Configure env
 dotenv.config();
 
-//database config
+// Database config
 connectDB();
-//rest object
-const app = express()
 
-//middlewares
+// Rest object
+const app = express();
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-//routes
+// Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
-app.use("/api/v1/product", productRoutes)
+app.use("/api/v1/product", productRoutes);
 
-//rest api
-app.get('/',(req,res) => {
-    res.send("<h1>welcome to ecommerce app</h1>");
+// Rest API
+app.get('/', (req, res) => {
+    res.send("<h1>Welcome to the E-commerce App</h1>");
 });
 
-//port
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build'))); // Adjust path as needed
+
+// The catch-all handler: for any request that doesn't match one above, send back index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html')); // Adjust path as needed
+});
+
+// Port
 const PORT = process.env.PORT || 8080;
 
-//run listen
-app.listen(PORT,() => {
+// Run listen
+app.listen(PORT, () => {
     console.log(`Server Running on ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white);
 });
