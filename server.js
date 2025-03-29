d3eb8667ae6de9e224b1eb39f55d68ddc9d67c95
Mohammed Afzal <78775949+Afzal-spec@users.noreply.github.com>
2025-03-29 22:40:53 +0530

    Trying to fix the 404 bug

--- server.js
+++ server.js
@@ -1,39 +1,48 @@
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
-import morgan from 'morgan';
+import morgan from "morgan";
 import connectDB from "./config/db.js";
-import authRoutes from './routes/authRoute.js'
-import categoryRoutes from './routes/categoryRoutes.js'
-import productRoutes from './routes/productRoutes.js'
-import cors from 'cors'
-//configure env
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
+import path from "path"; // Import path module
+
+// Configure env
 dotenv.config();
 
-//database config
+// Database config
 connectDB();
-//rest object
-const app = express()
 
-//middlewares
+// Rest object
+const app = express();
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-//routes
+// Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
-app.use("/api/v1/product", productRoutes)
+app.use("/api/v1/product", productRoutes);
 
-//rest api
-app.get('/',(req,res) => {
-    res.send("<h1>welcome to ecommerce app</h1>");
+// Serve frontend build files
+const __dirname = path.resolve(); // Get root directory
+app.use(express.static(path.join(__dirname, "/frontend/build"))); // Serve React build
+
+// Handle all other routes by serving index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
 });
 
-//port
+// Port
 const PORT = process.env.PORT || 8080;
 
-//run listen
-app.listen(PORT,() => {
-    console.log(`Server Running on ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white);
+// Run server
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white
+  );
 });
